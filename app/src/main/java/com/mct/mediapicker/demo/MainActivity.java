4cebb651efe35e0099f5809dfbf2906df99cc86f
SonNguyen <son15052002@gmail.com>
2024-12-11 02:57:26 +0700

    update: demo use

--- app/src/main/java/com/mct/mediapicker/demo/MainActivity.java
+++ app/src/main/java/com/mct/mediapicker/demo/MainActivity.java
@@ -11,41 +11,51 @@ import androidx.core.view.WindowInsetsCompat;
 import com.mct.mediapicker.M3ThemeStrategy;
 import com.mct.mediapicker.MediaPicker;
 import com.mct.mediapicker.MediaPickerOption;
+import com.mct.mediapicker.demo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        findViewById(R.id.tvHello).setOnClickListener(v -> {
-            MediaPicker.pick(getSupportFragmentManager(), new MediaPickerOption.Builder()
-                    // @formatter:off
-                    /*mode*/
-                    //.single(uri -> {})
-                    .multi(uris -> {})
-                    //.multiExact(uris -> {}, 3)
-                    //.multiRange(uris -> {}, 1, 10)
-                    // @formatter:on
-
-                    /*type*/
-                    .image()
-                    //.video()
-                    //.all()
-
-                    /*theme*/
-                    .themeStrategy(M3ThemeStrategy.DYNAMIC)
-
-                    /*build*/
-                    .build()
-            );
+        binding.btnStart.setOnClickListener(v -> {
+            MediaPicker.pick(getSupportFragmentManager(), createOption());
         });
+    }
+
+    private MediaPickerOption createOption() {
+        MediaPickerOption.Builder builder = new MediaPickerOption.Builder();
+        // @formatter:off
+
+        // type
+        if (binding.rbImage.isChecked()) builder.image();
+        if (binding.rbVideo.isChecked()) builder.video();
+        if (binding.rbAll.isChecked()) builder.all();
+
+        // mode
+        if (binding.rbSingle.isChecked()) builder.single(uri -> {});
+        if (binding.rbMulti.isChecked()) builder.multi(uris -> {});
+        if (binding.rbMultiExact.isChecked()) builder.multiExact(uris -> {}, 5);
+        if (binding.rbMultiRange.isChecked()) builder.multiRange(uris -> {}, 1, 10);
+
+        // theme
+        if (binding.rbDefault.isChecked()) builder.themeStrategy(M3ThemeStrategy.DEFAULT);
+        if (binding.rbInherit.isChecked()) builder.themeStrategy(M3ThemeStrategy.INHERIT);
+        if (binding.rbDynamic.isChecked()) builder.themeStrategy(M3ThemeStrategy.DYNAMIC);
+        if (binding.rbInheritOrDynamic.isChecked()) builder.themeStrategy(M3ThemeStrategy.INHERIT_OR_DYNAMIC);
+        if (binding.rbDynamicOrInherit.isChecked()) builder.themeStrategy(M3ThemeStrategy.DYNAMIC_OR_INHERIT);
+
+        // @formatter:on
+        return builder.build();
     }
 }

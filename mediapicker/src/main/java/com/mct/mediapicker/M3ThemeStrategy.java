43355d6fe7b16ccfca606dff8e7bbe609851e297
MCT <son15052002@gmail.com>
2024-12-10 17:37:08 +0700

    update: fix M3ThemeStrategy

--- mediapicker/src/main/java/com/mct/mediapicker/M3ThemeStrategy.java
+++ mediapicker/src/main/java/com/mct/mediapicker/M3ThemeStrategy.java
@@ -6,37 +6,38 @@ import android.content.res.Resources;
 import android.util.TypedValue;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.StyleRes;
 import androidx.appcompat.view.ContextThemeWrapper;
 
 import com.google.android.material.color.DynamicColors;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Resolves material3 theme for the media picker.
  */
 public abstract class M3ThemeStrategy {
 
+    /* --- base strategies --- */
+
     /**
      * Default theme from this library
      */
-    public static final M3ThemeStrategy DEFAULT = new M3ThemeStrategy() {
-        @NonNull
-        @Override
-        public Context wrapContext(@NonNull Context context) {
-            return fallback(context);
-        }
-    };
+    public static M3ThemeStrategy DEFAULT = theme(R.style.PhotoPickerTheme);
 
     /**
      * Inherit context theme or fallback to default
      */
     public static final M3ThemeStrategy INHERIT = new M3ThemeStrategy() {
-        @NonNull
         @Override
-        public Context wrapContext(@NonNull Context context) {
-            if (isMaterial3Theme(context.getTheme())) {
-                return context;
-            }
-            return fallback(context);
+        protected @NonNull Context handle(@NonNull Context context) {
+            return context;
+        }
+
+        @Override
+        protected boolean canHandle(@NonNull Context context) {
+            return isMaterial3Theme(context.getTheme());
         }
     };
 
@@ -44,31 +45,23 @@ public abstract class M3ThemeStrategy {
      * Dynamic color theme or fallback to default
      */
     public static final M3ThemeStrategy DYNAMIC = new M3ThemeStrategy() {
-        @NonNull
         @Override
-        public Context wrapContext(@NonNull Context context) {
-            if (DynamicColors.isDynamicColorAvailable()) {
-                return DynamicColors.wrapContextIfAvailable(context);
-            }
-            return fallback(context);
+        protected @NonNull Context handle(@NonNull Context context) {
+            return DynamicColors.wrapContextIfAvailable(context);
+        }
+
+        @Override
+        protected boolean canHandle(@NonNull Context context) {
+            return DynamicColors.isDynamicColorAvailable();
         }
     };
 
     /**
-     * Custom theme or fallback to default
+     * Custom material3 theme or fallback to default
      */
     @NonNull
-    public static M3ThemeStrategy CUSTOM(int m3ThemeResId) {
-        return new M3ThemeStrategy() {
-            @NonNull
-            @Override
-            public Context wrapContext(@NonNull Context context) {
-                if (isMaterial3Theme(m3ThemeResId)) {
-                    return new ContextThemeWrapperStrategy(m3ThemeResId).wrapContext(context);
-                }
-                return fallback(context);
-            }
-        };
+    public static M3ThemeStrategy CUSTOM(@StyleRes int m3ThemeResId) {
+        return theme(m3ThemeResId);
     }
 
     /* --- mix strategies --- */
@@ -76,55 +69,19 @@ public abstract class M3ThemeStrategy {
     /**
      * Inherit context theme or fallback to dynamic color
      */
-    public static final M3ThemeStrategy INHERIT_OR_DYNAMIC = new M3ThemeStrategy() {
-        @NonNull
-        @Override
-        public Context wrapContext(@NonNull Context context) {
-            return INHERIT.wrapContext(context);
-        }
-
-        @NonNull
-        @Override
-        protected Context fallback(@NonNull Context context) {
-            return DYNAMIC.wrapContext(context);
-        }
-    };
+    public static final M3ThemeStrategy INHERIT_OR_DYNAMIC = composite(INHERIT, DYNAMIC);
 
     /**
      * Dynamic color theme or fallback to inherit
      */
-    public static final M3ThemeStrategy DYNAMIC_OR_INHERIT = new M3ThemeStrategy() {
-        @NonNull
-        @Override
-        public Context wrapContext(@NonNull Context context) {
-            return DYNAMIC.wrapContext(context);
-        }
-
-        @NonNull
-        @Override
-        protected Context fallback(@NonNull Context context) {
-            return INHERIT.wrapContext(context);
-        }
-    };
+    public static final M3ThemeStrategy DYNAMIC_OR_INHERIT = composite(DYNAMIC, INHERIT);
 
     /**
      * Inherit context theme or fallback to custom
      */
     @NonNull
     public static M3ThemeStrategy INHERIT_OR_CUSTOM(int m3ThemeResId) {
-        return new M3ThemeStrategy() {
-            @NonNull
-            @Override
-            public Context wrapContext(@NonNull Context context) {
-                return INHERIT.wrapContext(context);
-            }
-
-            @NonNull
-            @Override
-            protected Context fallback(@NonNull Context context) {
-                return CUSTOM(m3ThemeResId).wrapContext(context);
-            }
-        };
+        return composite(INHERIT, CUSTOM(m3ThemeResId));
     }
 
     /**
@@ -132,35 +89,79 @@ public abstract class M3ThemeStrategy {
      */
     @NonNull
     public static M3ThemeStrategy DYNAMIC_OR_CUSTOM(int m3ThemeResId) {
+        return composite(DYNAMIC, CUSTOM(m3ThemeResId));
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Class methods
+    ///////////////////////////////////////////////////////////////////////////
+
+    @NonNull
+    public final Context wrapContext(@NonNull Context context) {
+        return composite(this, DEFAULT).handle(context);
+    }
+
+    @NonNull
+    protected abstract Context handle(@NonNull Context context);
+
+    protected abstract boolean canHandle(@NonNull Context context);
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Helper methods
+    ///////////////////////////////////////////////////////////////////////////
+
+    @NonNull
+    private static M3ThemeStrategy theme(@StyleRes int m3ThemeResId) {
         return new M3ThemeStrategy() {
-            @NonNull
             @Override
-            public Context wrapContext(@NonNull Context context) {
-                return DYNAMIC.wrapContext(context);
+            protected @NonNull Context handle(@NonNull Context context) {
+                return new ContextThemeWrapper(context, m3ThemeResId);
             }
 
-            @NonNull
             @Override
-            protected Context fallback(@NonNull Context context) {
-                return CUSTOM(m3ThemeResId).wrapContext(context);
+            protected boolean canHandle(@NonNull Context context) {
+                return isMaterial3Theme(context, m3ThemeResId);
             }
         };
     }
 
-    /* --- class methods --- */
-
     @NonNull
-    public abstract Context wrapContext(@NonNull Context context);
+    private static M3ThemeStrategy composite(M3ThemeStrategy... strategies) {
+        return new M3ThemeStrategy() {
+            @Override
+            protected @NonNull Context handle(@NonNull Context context) {
+                for (M3ThemeStrategy strategy : strategies) {
+                    if (strategy.canHandle(context)) {
+                        return strategy.handle(context);
+                    }
+                }
+                return context;
+            }
 
-    @NonNull
-    protected Context fallback(@NonNull Context context) {
-        return new ContextThemeWrapperStrategy(R.style.PhotoPickerTheme).wrapContext(context);
+            @Override
+            protected boolean canHandle(@NonNull Context context) {
+                for (M3ThemeStrategy strategy : strategies) {
+                    if (strategy.canHandle(context)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        };
     }
 
-    private static boolean isMaterial3Theme(int themeRes) {
-        Resources.Theme theme = Resources.getSystem().newTheme();
+    private static final Map<Integer, Boolean> themeCache = new HashMap<>();
+
+    private static boolean isMaterial3Theme(Context context, int themeRes) {
+        Boolean result = themeCache.get(themeRes);
+        if (result != null) {
+            return result;
+        }
+        Resources.Theme theme = context.getResources().newTheme();
         theme.applyStyle(themeRes, true);
-        return isMaterial3Theme(theme);
+        result = isMaterial3Theme(theme);
+        themeCache.put(themeRes, result);
+        return result;
     }
 
     @SuppressLint("PrivateResource")
@@ -171,21 +172,6 @@ public abstract class M3ThemeStrategy {
             return typedValue.type == TypedValue.TYPE_INT_BOOLEAN && typedValue.data != 0;
         }
         return false;
-    }
-
-    private static class ContextThemeWrapperStrategy extends M3ThemeStrategy {
-
-        private final int m3ThemeResId;
-
-        private ContextThemeWrapperStrategy(int themeRes) {
-            m3ThemeResId = themeRes;
-        }
-
-        @NonNull
-        @Override
-        public Context wrapContext(@NonNull Context context) {
-            return new ContextThemeWrapper(context, m3ThemeResId);
-        }
     }
 
 }
